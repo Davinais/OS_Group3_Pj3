47a30829e00ba5d81be262d154288027d267dfdf
Oscarkai9139 <deekai9139@gmail.com>
2019-06-10 01:28:33 +0800

    Feat: Add structs and funcs
    csim.c:
    add struct cache_t result_t
    add func size_of_cache searchCache addCache addr2tag load store

--- cachelab/csim.c
+++ cachelab/csim.c
@@ -3,6 +3,87 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <getopt.h>
+#include <math.h>
+#include <stdbool.h>
+
+int cache_size;
+int set_size;
+int line_len;
+int block_size;
+
+typedef struct cache{
+    uint64_t tag;
+    struct cache* next;
+}cache_t;
+
+typedef struct result{
+    bool miss;
+    bool hit;
+    bool eviction;
+}result_t;
+
+int size_of_cache(int set_size, int line_len, int block_size){
+    return pow(2, (set_size + line_len + block_size));
+}
+
+cache_t* searchCache(cache_t* head, int tag){
+    if(head == NULL) return NULL;
+    if(head->tag == tag) return head;
+    return search(head->next, tag);
+}
+
+bool addCache(cache_t* head, int tag){
+    cache_t* new_cache = (cache_t*)malloc(sizeof(cache_t));
+    new_cache->tag = tag;
+    new_cache->next = NULL;
+
+    //add to back of the list
+    cache_t* temp = head;
+    int size_of_current_set = 0;
+    while(temp->next != NULL){
+        size_of_current_set ++;
+        temp = temp->next;
+    }
+
+    //add new node
+    temp = new_cache;
+    
+    //check valid cache
+    if(size_of_cache(size_of_current_set, line_len, block_size) < size_of_cache(set_size, line_len, block_size)){
+        return false; //no eviction
+    }
+    else {
+        //delete head
+        head = head->next;
+        return true;
+    }
+} 
+
+uint64_t addr2tag(int addr){
+    uint64_t tag = 0;
+    return tag;
+}
+
+result_t load(cache_t* head, int addr){
+    result_t ret = {false, false, false};
+    uint64_t tag = addr2tag(addr);
+    if(searchCache(head, tag) != NULL) ret.hit = true;
+    else {
+        ret.miss = true;
+        if(addCache(head, tag)) ret.eviction = true;
+    }
+}
+
+//same wtf? modify need to call load and store
+result_t store(cache_t* head, int addr){
+    result_t ret = {false, false, false};
+    uint64_t tag = addr2tag(addr);
+    if(searchCache(head, tag) != NULL) ret.hit = true;
+    else {
+        ret.miss = true;
+        if(addCache(head, tag)) ret.eviction = true;
+    }
+}
 
 void printHelp(char* name) {
     printf("Usage: %s [-hv] -s <num> -E <num> -b <num> -t <file>\n", name);
